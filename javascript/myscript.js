f40124d44863d6c6ef0dbc90d710e7ae43d2da27
dmswldi <eeeunzz20@gmail.com>
2020-09-25 17:25:48 +0900

    0925

---
+++ javascript/myscript.js
@@ -0,0 +1,12 @@
+// document.getElementsByTagName("h1")[0]
+function show(){
+    console.log("show function");
+}
+
+function $myfunc(){ // &_Aa1
+    console.log("my function");
+}
+
+function $(){
+    console.log("$ function");
+}
